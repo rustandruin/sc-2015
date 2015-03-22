d07d6d76129a5d8ad7b3df0602a4b819cb9e2749
Jey Kottalam <jey@kottalam.net>
2015-03-21 21:19:20 -0700

    adjust event logging

--- spark_msi/runspark.sh
+++ spark_msi/runspark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 start-all.sh
-spark-submit --conf spark.eventLog.enabled=true --conf spark.eventLog.dir=$SPARK_EVENTLOG_DIR --master $SPARKURL --executor-memory 32G --driver-memory 32G ./spark_msi.py
+spark-submit --conf pbs.jobId=$PBS_JOBID --conf spark.eventLog.enabled=true --conf spark.eventLog.dir=$SPARK_EVENTLOG_DIR --master $SPARKURL --executor-memory 32G --driver-memory 32G ./spark_msi.py
 stop-all.sh 
