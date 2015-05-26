a511ffbf441d014cdbd2989cec8003326fb43830
Jey Kottalam <jey@kottalam.net>
2015-05-26 14:54:29 -0700

    scala impl seems to work

--- cx_spark/src/main/scala/CX.scala
+++ cx_spark/src/main/scala/CX.scala
@@ -5,9 +5,11 @@ import org.apache.spark.SparkConf
 import org.apache.spark.mllib.linalg.{Matrices, DenseMatrix, Matrix, DenseVector, Vector}
 import org.apache.spark.mllib.linalg.distributed._
 import breeze.linalg.{Matrix => BM, DenseMatrix => BDM, DenseVector => BDV, Axis, qr, svd, sum, axpy, SparseVector => BSV}
+import math.{ceil, log}
 
 object CX {
   def fromBreeze(mat: BDM[Double]): DenseMatrix = {
+    // FIXME: does not support strided matrices (e.g. views)
     new DenseMatrix(mat.rows, mat.cols, mat.data, mat.isTranspose)
   }
 
@@ -25,6 +27,7 @@ object CX {
   }
 
   def transposeMultiply(mat: IndexedRowMatrix, rhs: DenseMatrix): DenseMatrix = {
+    require(mat.numRows == rhs.numRows)
     val rhsBrz = rhs.toBreeze.asInstanceOf[BDM[Double]]
     val result =
       mat.rows.treeAggregate(BDM.zeros[Double](mat.numCols.toInt, rhs.numCols))(
@@ -33,7 +36,7 @@ object CX {
           val rowBrz = row.vector.toBreeze.asInstanceOf[BSV[Double]]
           // performs a rank-1 update:
           //   U += outer(row.vector, rhs(row.index, ::))
-          for(ipos <- 0 until rowBrz.length) {
+          for(ipos <- 0 until rowBrz.index.length) {
             val i = rowBrz.index(ipos)
             val ival = rowBrz.data(ipos)
             for(j <- 0 until rhs.numCols) {
@@ -53,51 +56,59 @@ object CX {
   }
 
   def main(args: Array[String]) {
+    val name = "Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked"
+    //val name = "Lewis_Dalisay_Peltatum_20131115_PDX_Std_1"
+    val prefix = s"hdfs:///$name.mat"
     //val prefix = "hdfs:///"
-    //val name = "Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked-100x100"
-    val name = "Lewis_Dalisay_Peltatum_20131115_PDX_Std_1"
-    val prefix = s"/home/jey/proj/openmsi/data/2014Nov15_PDX_IMS_imzML/$name"
+    //val prefix = s"/home/jey/proj/openmsi/data/2014Nov15_PDX_IMS_imzML/$name"
     val inpath = s"$prefix/$name.mat.csv"
     val conf = new SparkConf().setAppName("CX")
+    conf.set("spark.driver.maxResultSize", "32g")
     val sc = new SparkContext(conf)
 
     /* params */
-    val numIters = 1
-    val rank = 4      // rank of approximation
-    val slack = rank  // extra slack to improve the approximation
+    val numIters = 2
+    // rank of approximation
+    val rank = 8
+    // extra slack to improve the approximation
+    val slack = ceil(log(rank)/log(2)).toInt
+    val k = rank + slack
 
     /* load matrix */
     val nonzeros = sc.textFile(inpath).map(_.split(",")).
           map(x => new MatrixEntry(x(0).toLong, x(1).toLong, x(2).toDouble))
-    val coomat = new CoordinateMatrix(nonzeros, 951, 781210) // FIXME: magics
+    //val coomat = new CoordinateMatrix(nonzeros, 951, 781210) // FIXME: magics
+    //val coomat = new CoordinateMatrix(nonzeros, 9574, 3743324) // FIXME: magics
+    val coomat = new CoordinateMatrix(nonzeros, 131048, 8258911)
     val mat = coomat.toIndexedRowMatrix()
     mat.rows.cache()
 
     /* perform randomized SVD */
-    var Y = gaussianProjection(mat, rank + slack).toBreeze
+    var Y = gaussianProjection(mat, k).toBreeze
     for(i <- 0 until numIters) {
       Y = multiplyGramianBy(mat, fromBreeze(Y)).toBreeze.asInstanceOf[BDM[Double]]
     }
-    assert(Y.cols == rank + slack)
-    assert(Y.rows == 951)
-    val Q = qr.justQ(Y)
+    val Q = qr.reduced.justQ(Y)
+    assert(Q.cols == k)
     val B = transposeMultiply(mat, fromBreeze(Q)).transpose.toBreeze.asInstanceOf[BDM[Double]]
     val Bsvd = svd.reduced(B)
     val U = (Q * Bsvd.U).apply(::, 0 until rank)
     val S = Bsvd.S(0 until rank)
-    val Vt = Bsvd.Vt(0 until rank, ::)
+    val V = Bsvd.Vt(0 until rank, ::).t
 
     /* compute leverage scores */
     val rowlev = sum(U :^ 2.0, Axis._1)
     val rowp = rowlev / rank.toDouble
-    val collev = sum(Vt :^ 2.0, Axis._1)
+    val collev = sum(V :^ 2.0, Axis._1)
     val colp = collev / rank.toDouble
 
     println("S:\n")
     println(S)
+    /*
     println("RowP:")
     println(rowp)
     println("\nColP:")
     println(colp)
+    */
   }
 }
