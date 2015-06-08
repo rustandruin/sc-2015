0c8081060a787def97ba278a4274565e731dc1aa
Jey Kottalam <jey@kottalam.net>
2015-06-08 13:21:41 -0700

    revert runcx.sh

--- cx_spark/src/runcx.sh
+++ cx_spark/src/runcx.sh
@@ -1,10 +1,12 @@
 spark-submit --verbose \
   --driver-memory 32G \
   --conf spark.driver.maxResultSize=4g \
-  $1 \
-  genmat s3n://amp-jey/sc-2015/Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat/Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat.csv \
-  hdfs:///test.out
+  --conf spark.task.maxFailures=1 \
+  target/scala-2.10/heromsi-assembly-0.0.1.jar \
+  csv hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat.csv \
+  8258911 131048 \
+  cx-out.json \
+  16 2 1
 
-#  genmat hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-smoothed-mz=437.11407-sd=0.05.rawmat.csv.gz \
 # notes
 #  idxrow hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.rowmat \
