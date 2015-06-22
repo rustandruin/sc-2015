9b781b8ddad5d47eddf92cba30171841452904c6
Jey Kottalam <jey@kottalam.net>
2015-06-22 13:31:36 -0700

    update runcx.sh for cx-32-4-4 on hero dataset

--- cx_spark/src/runcx.sh
+++ cx_spark/src/runcx.sh
@@ -1,11 +1,13 @@
 spark-submit --verbose \
   --driver-memory 96G \
+  --conf spark.eventLog.enabled=true \
+  --conf spark.eventLog.dir=$PWD/eventlogs \
   --conf spark.driver.maxResultSize=64g \
   $1 \
-  csv hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat.csv \
-  8258911 131048 \
-  smdump-24-4-4-orth.out \
-  24 4 4
+  csv hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-smoothed-mz=437.11407-sd=0.05.mat.df \
+  0 0 \
+  cx-32-4-4-orth.out \
+  32 4 4
 
 #  genmat \
 #  hdfs:///Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat/Lewis_Dalisay_Peltatum_20131115_hexandrum_1_1-masked.mat.csv \
